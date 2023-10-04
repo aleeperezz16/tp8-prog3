2eb023d00f41a320999af93deb1ca94869449da1
Alejo <alejoperezherrero@gmail.com>
2023-10-04 10:32:02 -0300

    Agregar archivos de proyecto.

---
+++ DAO/AccesoDatos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class AccesoDatos
+    {
+        public string Ruta { get; set; }
+        public AccesoDatos()
+        {
+
+        }
+
+        public AccesoDatos(string baseDeDatos)
+        {
+            Ruta = $"Data Source=localhost\\sqlexpress; Initial Catalog={baseDeDatos}; Integrated Security=True";
+        }
+    }
+}
