688785e6ce0f23e76d3992808cc64ac475991f9c
Alejo <alejoperezherrero@gmail.com>
2023-10-05 19:31:59 -0300

    Entidad "Sucursal"

---
+++ Entidades/Sucursal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Sucursal
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int IdHorario { get; set; }
+        public int IdProvincia { get; set; }
+        public string Direccion { get; set; }
+        public string UrlImagen { get; set; }
+
+        public Sucursal()
+        {
+
+        }
+
+        public Sucursal(string nombre, string descripcion, int idHorario, int idProvincia, string direccion, string urlImagen)
+        {
+            Nombre = nombre;
+            Descripcion = descripcion;
+            IdHorario = idHorario;
+            IdProvincia = idProvincia;
+            Direccion = direccion;
+            UrlImagen = urlImagen;
+        }
+    }
+}
